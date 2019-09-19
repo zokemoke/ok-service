68992e6ac4b6edb1dccc2dcd92f60d8e4d4c9e59
Oak Isarapong <ozaaaaa@gmail.com>
2019-09-19 14:30:53 +0700

    add project

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const JsonDB  = require('node-json-db').JsonDB
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config
+var db = new JsonDB(new Config("token", true, false, '/'));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.post('/token', function (req, res) {
+    let reqBody = req.body
+    db.push('/'+reqBody.local,reqBody.data)
+    res.send(reqBody)
+});
+
+app.get('/token', (req, res) => {
+    let resData = db.getData("/UDH")
+    res.send(resData)
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
