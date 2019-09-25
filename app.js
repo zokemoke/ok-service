4ff31147df70dd709b9fccfc87b1075f6ecf4f5a
Oak Isarapong <ozaaaaa@gmail.com>
2019-09-25 12:55:26 +0700

    add Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const nhso = require("./routes/nhso");
 
 mongoose.connect(db, { dbName: "nhso" });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods","GET,POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
